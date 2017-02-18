86b58e1d015e853cf6878f3fd2f73d9e91712046
Don <yudongxiao@live.com>
2017-02-18 09:12:44 -0500

    while loop and range

--- firstTest.py
+++ firstTest.py
@@ -1,1 +1,14 @@
-print 'hello world'
+x=0
+while x<10:
+    print 'x is currently:',x
+    x+=1
+    if x==3:
+        print 'Hey x equals 3!'
+    else:
+        print 'continuing...'
+        continue
+else:
+    print 'All Done!'
+
+for num in range(10):
+    print num
